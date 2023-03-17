b55085f7d3564dccb16b261e53b225503af41ff1
Max <Asket-axe@mail.ru>
2023-03-17 22:55:08 +0300

    правки 1.0

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
 
-    private final Connection connection = new Util().getConnection();
+    private final Util util = new Util();
+
     public UserDaoJDBCImpl() {
 
     }
@@ -20,7 +21,8 @@ public class UserDaoJDBCImpl implements UserDao {
                 + " name varchar(255),"
                 + " lastName varchar(255),"
                 + " age integer)";
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = util.getConnection();
+             Statement statement = connection.createStatement()) {
             statement.executeUpdate(sql);
         } catch (SQLException e) {
             System.out.println("не удалось создать таблицу");
@@ -29,7 +31,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void dropUsersTable() {
         final String sql = "DROP TABLE IF EXISTS Users";
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = util.getConnection();
+             Statement statement = connection.createStatement()) {
             statement.executeUpdate(sql);
         } catch (SQLException e) {
             System.out.println("не удалось удалить таблицу");
@@ -38,7 +41,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void saveUser(String name, String lastName, byte age) {
         final String sql = "INSERT INTO Users (name, lastName, age) VALUES (?, ?, ?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        try (Connection connection = util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
@@ -51,7 +55,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void removeUserById(long id) {
         final String sql = "DELETE FROM Users WHERE id=?";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        try (Connection connection = util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -62,7 +67,8 @@ public class UserDaoJDBCImpl implements UserDao {
     public List<User> getAllUsers() {
         List<User> userList = new ArrayList<>();
         final String sql = "SELECT name, lastName, age FROM Users";
-        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(sql)) {
+        try (Connection connection = util.getConnection();
+             Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(sql)) {
             while (resultSet.next()) {
                 User user = new User();
                 user.setName(resultSet.getString("name"));
@@ -81,7 +87,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void cleanUsersTable() {
         final String sql = "DELETE FROM Users";
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = util.getConnection();
+             Statement statement = connection.createStatement()) {
             statement.executeUpdate(sql);
         } catch (SQLException e) {
             System.out.println("не удалось очистить таблицу");
